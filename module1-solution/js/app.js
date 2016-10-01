43c445c5796ba88bfe50e2acb270e5b491d0969f
ram anam <ram@introp.net>
2016-10-01 23:18:50 +1000

    Showing message in different colour based on state

--- module1-solution/js/app.js
+++ module1-solution/js/app.js
@@ -10,14 +10,17 @@
 
         $scope.lunchItems = '';
         $scope.message = '';
+        $scope.inputValid = null;
 
         $scope.checkIfTooMuch = function(){
             //Check if empty
             if($scope.lunchItems == ''){
                 $scope.message = 'Please enter data first';
+                $scope.inputValid = false;
                 return;
             }
 
+            $scope.inputValid = true;
             var numberOfItems = $scope.lunchItems.split(',').length;
             $scope.message = numberOfItems <= 3 ? 'Enjoy!' : 'Too much!';
         }
